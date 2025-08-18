aad360b94d38c79ceabb5dbfd4f7794496aa81a4
Princesingh987 <168814139+Princesingh987@users.noreply.github.com>
2025-08-18 21:44:36 +0530

    code pushed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const express = require("express");
+const express =require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
